53974ea36c32c1e4b9442f4b0903f6e9ee2fc81f
Edweiss200 <mykhoedovs@gmail.com>
2026-09-03 16:04:00 +0700

    task1

---
+++ Task1/Task1/OilCost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class OilCost
+    {
+        public string OilType { get; set; }
+        public DateTime Date { get; set; }
+        public double Cost { get; set; }
+
+        public OilCost(string oilType, DateTime date, double cost) 
+        {
+            OilType = oilType;
+            Date = date;
+            Cost = cost;
+        }
+
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string input_data = Console.ReadLine();
+            string[] input_data_list = input_data.Split(' ');
+            string OilType = input_data_list[0].Trim('"');
+            DateTime dt = DateTime.ParseExact(input_data_list[1], "yyyy.mm.dd", null);
+            double cost = Double.Parse(input_data_list[2]);
+
+            OilCost oilcost = new OilCost(OilType, dt, cost);
+
+            Console.WriteLine($"Тип топлива {oilcost.OilType}");
+            Console.WriteLine($"Дата {oilcost.Date}");
+            Console.WriteLine($"Стоимость {oilcost.Cost}");
+
+        }
+    }
+}
